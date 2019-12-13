be7815997e4a127e199c77854b9b7c9e74d1b049
Alex Show <zmpc@mail.ru>
2019-12-14 00:50:31 +0300

    Added line11 by Alex2

--- src/main/java/com/zmpc/app/App.java
+++ src/main/java/com/zmpc/app/App.java
@@ -42,6 +42,8 @@ public class App {
         Person person = new Person();
         person.setName("Alex");
         System.out.println(person);
+
+        System.out.println("New Line 11");
     }
 
 }
