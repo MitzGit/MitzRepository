89504d1a3b328564661b85f56177485b4ab6a00d
Alex Show <zmpc@mail.ru>
2019-12-13 22:56:53 +0300

    Added Person entity

--- src/main/java/com/zmpc/app/App.java
+++ src/main/java/com/zmpc/app/App.java
@@ -1,9 +1,13 @@
 package com.zmpc.app;
 
+import com.zmpc.entities.Person;
+
 /**
  * Hello world!
  */
 public class App {
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
@@ -14,10 +18,16 @@ public class App {
         System.out.println("Added new line 3.1 from Develop");
 
         System.out.println("Added new line 4.1 from Develop");
-		
-		System.out.println("Some modifications");
+
+        System.out.println("Some modifications");
 
         System.out.println("Added new lines of code");
+
+
+        Person person = new Person();
+        person.setName("Alex");
+        person.setSurname("Show");
+        person.setAge(30);
     }
 
 }
